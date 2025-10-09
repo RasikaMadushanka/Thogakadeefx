f3674696f3c4b2450b8fb00ff4ffad615e1f80ff
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2025-10-09 22:46:06 +0530

    add login gui

---
+++ src/main/java/Controller/LoginController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private TextField txtUserPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
